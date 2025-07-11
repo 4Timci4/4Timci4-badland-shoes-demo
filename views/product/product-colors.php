d87e6905cdddb28e208e5d8a11ba8e035e3f90c6
4Timci4 <timcilol@gmail.com>
2025-07-11 16:50:38 +0300

    .

--- views/product/product-colors.php
+++ views/product/product-colors.php
@@ -14,16 +14,15 @@
             // Bu renk seçili mi kontrol et
             $is_selected = ($selected_color_id == $color['id']);
         ?>
-            <button type="button"
+            <a href="<?php echo htmlspecialchars($color_url); ?>"
                class="color-option w-12 h-12 rounded-full border-2 <?php echo $is_selected ? 'border-secondary' : 'border-gray-300'; ?> hover:border-secondary transition-all duration-200 block relative"
                style="background-color: <?php echo htmlspecialchars($color['hex_code']); ?>"
                data-color-id="<?php echo $color['id']; ?>"
                data-color-name="<?php echo htmlspecialchars($color['name']); ?>"
                data-color-slug="<?php echo htmlspecialchars($color_slug); ?>"
-               data-color-url="<?php echo htmlspecialchars($color_url); ?>"
                title="<?php echo htmlspecialchars($color['name']); ?>">
                 <span class="color-tooltip absolute -bottom-8 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-xs px-2 py-1 rounded opacity-0 transition-opacity"><?php echo htmlspecialchars($color['name']); ?></span>
-            </button>
+            </a>
         <?php endforeach; ?>
     </div>
     <p class="text-sm text-gray-600 mt-2">Seçili renk: <span id="selected-color"><?php
@@ -38,9 +37,4 @@
         }
         echo $selected_color_name;
     ?></span></p>
-</div>
-
-<!-- Renk-Beden Grid'i -->
-<div id="color-size-grid" class="mt-4">
-    <!-- JavaScript ile doldurulacak -->
 </div>
